71080d8f8d51d4c15eedc8afefe9887cbca9291c
Takuma <takuma012011@gmail.com>
2020-06-03 10:53:27 +0900

    repai

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root "clean_blogs#index"
+
   resources :members
   resources :posts
   resources :clean_blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "clean_blogs#index"
 get  'about' 	=> 'clean_blogs#about'
 # get  'post' 	=> 'clean_blogs#post'
 get  'contact' 	=> 'clean_blogs#contact'

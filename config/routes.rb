4458c29627bc184849758ada2175dd5ea54eceb1
Takuma <takuma012011@gmail.com>
2020-06-03 13:44:07 +0900

    router repair

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
   resources :members
   resources :posts
   resources :clean_blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root "clean_blogs#index"
+get '/' => "clean_blogs#index"
 get  'about' 	=> 'clean_blogs#about'
 get  'contact' 	=> 'clean_blogs#contact'
 get 'result' => 'clean_blogs#result'
